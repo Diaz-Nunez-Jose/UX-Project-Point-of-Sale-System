b1f12f21252e076b3bba28840c4951ff84f90535
Jose Diaz-Nunez <dia15012@byui.edu>
2018-03-19 15:26:22 -0600

    Updated order-creation.php

--- order-creation.js
+++ order-creation.js
@@ -63,28 +63,28 @@ function handleStyleUp(element) {
 	}
 }
 
-function styleMenuButtons() {
-    var currentCategory = <?php echo $_SESSION["category"]; ?>;
+// function styleMenuButtons() {
+//     var currentCategory = <?php echo $_SESSION["category"]; ?>;
 
-    /*        
-        $_SESSION["category"] = "Drinks";
-    } else if (isset($_POST["appetizers"])) {
-        $_SESSION["category"] = "Appetizers";
-    } else if (isset($_POST["entrees"])) {
-        $_SESSION["category"] = "Entrees";
-    } else if (isset($_POST["desserts"])) {
-        $_SESSION["category"] = "Desserts";
-    } else if (isset($_POST["sides"])) {
-        $_SESSION["category"] = "Sides";
-    } else if (isset($_POST["promos"])) {
-        $_SESSION["category"] = "Promos";
-    */
+            
+//         $_SESSION["category"] = "Drinks";
+//     } else if (isset($_POST["appetizers"])) {
+//         $_SESSION["category"] = "Appetizers";
+//     } else if (isset($_POST["entrees"])) {
+//         $_SESSION["category"] = "Entrees";
+//     } else if (isset($_POST["desserts"])) {
+//         $_SESSION["category"] = "Desserts";
+//     } else if (isset($_POST["sides"])) {
+//         $_SESSION["category"] = "Sides";
+//     } else if (isset($_POST["promos"])) {
+//         $_SESSION["category"] = "Promos";
+    
 
-    if(currentCategory == "Drinks") {
-        handleStyleDown(document.getElementById("apps"));
-        handleStyleDown(document.getElementById("entrees"));
-        handleStyleDown(document.getElementById("desserts"));
-        handleStyleDown(document.getElementById("sides"));
-        handleStyleDown(document.getElementById("promos"));
-    }
-}
+//     if(currentCategory == "Drinks") {
+//         handleStyleDown(document.getElementById("apps"));
+//         handleStyleDown(document.getElementById("entrees"));
+//         handleStyleDown(document.getElementById("desserts"));
+//         handleStyleDown(document.getElementById("sides"));
+//         handleStyleDown(document.getElementById("promos"));
+//     }
+// }
